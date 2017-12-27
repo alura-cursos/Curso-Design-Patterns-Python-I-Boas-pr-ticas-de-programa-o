5e56fdaab51e592251c8335841f7aa982447ac5c
Andre Chaves <andrechavesg@gmail.com>
2017-12-27 09:04:46 -0200

    capitulo5

---
+++ nota_fiscal.py
@@ -0,0 +1,64 @@
+# -*- coding: UTF-8 -*-
+from datetime import date
+
+class Item(object):
+
+    def __init__(self, descricao, valor):
+        self.__descricao = descricao
+        self.__valor = valor
+
+    @property
+    def descricao(self):
+        return self.__descricao
+
+    @property
+    def valor(self):
+        return self.__valor
+
+class Nota_fiscal(object):
+
+    def __init__(self, razao_social, cnpj, itens, data_de_emissao, detalhes):
+        self.__razao_social = razao_social
+        self.__cnpj = cnpj
+        self.__data_de_emissao = data_de_emissao
+        if len(detalhes) > 20:
+            raise Exception('Detalhes da nota não pode ter mais do que 20 caracteres')
+        self.__detalhes = detalhes
+        self.__itens = itens
+
+    @property
+    def razao_social(self):
+        return self.__razao_social
+
+    @property
+    def cnpj(self):
+        return self.__cnpj
+
+    @property
+    def data_de_emissao(self):
+        return self.__data_de_emissao
+
+    @property
+    def detalhes(self):
+        return self.__detalhes
+
+if __name__ == '__main__':
+
+    itens=[
+        Item(
+            'ITEM A',
+            100
+        ),
+        Item(
+            'ITEM B',
+            200
+        )
+    ]
+
+    nota_fiscal = Nota_fiscal(
+        'FHSA Limitada',
+        '012345678901234',
+        itens,
+        date.today(),
+        ''
+    )
--- orcamento.py
+++ orcamento.py
@@ -1,15 +1,117 @@
 # -*- coding: UTF-8 -*-
+from abc import ABCMeta, abstractmethod
+
+class Estado_de_um_orcamento(object):
+
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def aplica_desconto_extra(self, orcamento):
+        pass
+
+    @abstractmethod
+    def aprova(self, orcamento):
+        pass
+
+    @abstractmethod
+    def reprova(self, orcamento):
+        pass
+
+    @abstractmethod
+    def finaliza(self, orcamento):
+        pass
+
+class Em_aprovacao(Estado_de_um_orcamento):
+
+    def aplica_desconto_extra(self, orcamento):
+        orcamento.adiciona_desconto_extra(orcamento.valor * 0.02)
+
+    def aprova(self, orcamento):
+        orcamento.estado_atual = Aprovado()
+
+    def reprova(self, orcamento):
+        orcamento.estado_atual = Reprovado()
+
+    def finaliza(self, orcamento):
+        raise Exception('Orçamento em aprovação não podem ir para finalizado')
+
+
+class Aprovado(Estado_de_um_orcamento):
+
+    def aplica_desconto_extra(self, orcamento):
+        orcamento.adiciona_desconto_extra(orcamento.valor * 0.05)
+
+    def aprova(self, orcamento):
+        raise Exception('Orçamento já está aprovado')
+
+    def reprova(self, orcamento):
+
+        raise Exception('Orçamento aprovados não podem ser reprovados')
+
+    def finaliza(self, orcamento):
+
+        orcamento.estado_atual = Finalizado()
+
+class Reprovado(Estado_de_um_orcamento):
+
+    def aplica_desconto_extra(self, orcamento):
+        raise Exception('Orçamentos reprovados não receberam desconto extra')
+
+    def aprova(self, orcamento):
+        raise Exception('Orçamento reprovado não pode ser aprovado')
+
+    def reprova(self, orcamento):
+        raise Exception('Orçamento reprovado não pode ser reprovado novamente')        
+
+    def finaliza(self, orcamento):
+        orcamento.estado = Finalizado()
+
+class Finalizado(Estado_de_um_orcamento):
+
+    def aplica_desconto_extra(self, orcamento):
+        raise Exception('Orçamentos finalizados não receberam desconto extra')
+
+    def aprova(self, orcamento):
+        raise Exception('Orçamentos finalizados não podem ser aprovados novamente')        
+
+    def reprova(self, orcamento):
+        raise Exception('Orçamentos finalizados não podem ser reprovados')
+
+    def finaliza(self, orcamento):
+        raise Exception('Orçamentos finalizados não podem ser finalizados novamente')        
+
+
 class Orcamento(object):
 
     def __init__(self):
         self.__itens = []
+        self.estado_atual = Em_aprovacao()
+        self.__desconto_extra = 0
+
+    def aprova(self):
+        self.estado_atual.aprova(orcamento)
+
+    def reprova(self):
+        self.estado_atual.reprova(orcamento)
+
+    def finaliza(self):
+        self.estado_atual.finaliza(orcamento)
+
+    def aplica_desconto_extra(self):
+
+        self.estado_atual.aplica_desconto_extra(self)
+
+    def adiciona_desconto_extra(self, desconto):
+
+        self.__desconto_extra+= desconto
+
 
     @property
     def valor(self):
         total = 0.0
         for item in self.__itens:
             total+= item.valor
-        return total
+        return total - self.__desconto_extra
 
     def obter_itens(self):
 
@@ -35,3 +137,15 @@ class Item(object):
     @property
     def nome(self):
         return self.__nome
+
+if __name__ == '__main__':
+
+    orcamento = Orcamento()
+    orcamento.adiciona_item(Item('ITEM - 1', 100))
+    orcamento.adiciona_item(Item('ITEM - 2', 50))
+    orcamento.adiciona_item(Item('ITEM - 3', 400))
+
+    print orcamento.valor
+    orcamento.aprova()
+    orcamento.reprova()
+
