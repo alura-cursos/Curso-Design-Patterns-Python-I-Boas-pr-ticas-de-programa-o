5d516dbb0655a63f78dea6f8de53f7a876701af7
Andre Chaves <andrechavesg@gmail.com>
2017-12-27 09:04:09 -0200

    capitulo4

--- calculador_de_impostos.py
+++ calculador_de_impostos.py
@@ -27,8 +27,14 @@ if __name__ == '__main__':
 	calculador.realiza_calculo(orcamento, ISS())
 	calculador.realiza_calculo(orcamento, ICMS())
 
+	print 'ISS com ICMS'
+	calculador.realiza_calculo(orcamento, ISS(ICMS()))
+
 	print 'ICPP e IKCV'
 	calculador.realiza_calculo(orcamento, ICPP())
 	calculador.realiza_calculo(orcamento, IKCV())
+
+	print 'ICPP com IKCV'
+	calculador.realiza_calculo(orcamento, ICPP(IKCV()))
 
 
--- impostos.py
+++ impostos.py
@@ -2,16 +2,33 @@
 
 from abc import ABCMeta, abstractmethod
 
-class Template_de_imposto_condicional(object):
+
+class Imposto(object):
+
+	def __init__(self, outro_imposto = None):
+		self.__outro_imposto = outro_imposto
+
+	def calculo_do_outro_imposto(self, orcamento):
+		
+		if self.__outro_imposto is None:
+			return 0
+		else:
+			return self.__outro_imposto.calcula(orcamento)
+
+	@abstractmethod		
+	def calcula(self, orcamento):
+		pass
+
+class Template_de_imposto_condicional(Imposto):
 
 	__metaclass__ = ABCMeta
 
 	def calcula(self, orcamento):
 
 		if self.deve_usar_maxima_taxacao(orcamento):
-			return self.maxima_taxacao(orcamento)
+			return self.maxima_taxacao(orcamento) + self.calculo_do_outro_imposto(orcamento)
 		else:
-			return self.minima_taxacao(orcamento)
+			return self.minima_taxacao(orcamento) + self.calculo_do_outro_imposto(orcamento)
 
 	@abstractmethod
 	def deve_usar_maxima_taxacao(self, orcamento):
@@ -25,17 +42,22 @@ class Template_de_imposto_condicional(object):
 	def minima_taxacao(self, orcamento):
 		pass
 
-class ISS(object):
+def IPVX(metodo_ou_funcao):
+	def wrapper(self, orcamento):
+		return metodo_ou_funcao(self, orcamento) + 50.0
+	return wrapper
+
+class ISS(Imposto):
 
 	def calcula(self, orcamento):
 
-		return orcamento.valor * 0.1
+		return orcamento.valor * 0.1 + self.calculo_do_outro_imposto(orcamento)
 
-class ICMS(object):
+class ICMS(Imposto):
 
 	def calcula(self, orcamento):
 
-		return orcamento.valor * 0.06
+		return orcamento.valor * 0.06 + self.calculo_do_outro_imposto(orcamento)
 
 class ICPP(Template_de_imposto_condicional):
 
